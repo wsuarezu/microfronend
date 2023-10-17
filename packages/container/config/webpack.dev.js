840212f2510eba99949ab81ee84ebe3fd5306fb6
William Suáres <william.suares@ucacue.edu.ec>
2023-10-17 10:19:07 -0500

    Created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpacgPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -20,9 +19,6 @@ const devConfig = {
       },
       shared: packageJson.dependencies,
     }),
-    new HtmlWebpacgPlugin({
-      template: './public/index.html'
-    })
   ]
 }
 
