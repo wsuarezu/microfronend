50cf581963ae69359906cfbfbc71bac76a4cef1a
William Suáres <william.suares@ucacue.edu.ec>
2023-11-27 11:23:22 -0500

    archivos fin de curso.

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el, { onNavigate, defaultHistory}) => {
-  const history = defaultHistory || createMemoryHistory();
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath}) => {
+  const history = 
+    defaultHistory || 
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
 
   if (onNavigate){
     history.listen(onNavigate);
   }
 
   ReactDOM.render(
-    <App history={history} />,
+    <App onSignIn={onSignIn} history={history} />,
     el
   );
 
